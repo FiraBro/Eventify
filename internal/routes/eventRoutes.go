37778fd4520e55902722cbbda5f595c620ac79ed
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-21 10:34:39 +0300

    add updfate functionality

--- internal/routes/eventRoutes.go
+++ internal/routes/eventRoutes.go
@@ -14,4 +14,6 @@ func SetupRoutes(r *gin.Engine, h *handlers.EventHandler) {
 	r.GET("/events", h.GetEvents)
 	r.GET("/events/:id", h.GetEventByID)
 	r.POST("/events", h.CreateEvent)
+	r.PUT("/events/:id", h.UpdateEvent)
+
 }
