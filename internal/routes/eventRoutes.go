6cc16a4e89b5f5c1fca71f80e9234a85a6e1b15f
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-22 09:44:07 +0300

    add versionning api

--- internal/routes/eventRoutes.go
+++ internal/routes/eventRoutes.go
@@ -7,14 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupEventRoutes(r *gin.Engine, eventHandler *handlers.EventHandler) {
+// Accept *gin.RouterGroup instead of *gin.Engine
+func SetupEventRoutes(rg *gin.RouterGroup, eventHandler *handlers.EventHandler) {
 	// Public routes
-	r.GET("/events", eventHandler.GetEvents)
-	r.GET("/events/:id", eventHandler.GetEventByID)
+	rg.GET("/events", eventHandler.GetEvents)
+	rg.GET("/events/:id", eventHandler.GetEventByID)
 
 	// Protected routes
-	authGroup := r.Group("/")
-	authGroup.Use(middlewares.JWTAuth())
+	authGroup := rg.Group("/")
+	authGroup.Use(middlewares.AuthMiddleware())
 	{
 		authGroup.POST("/events", eventHandler.CreateEvent)
 		authGroup.PUT("/events/:id", eventHandler.UpdateEvent)
