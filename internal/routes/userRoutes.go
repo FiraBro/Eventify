6b7a45fa508db9844a5152339e3030f3538236d6
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-22 09:44:34 +0300

    add versionning api

--- internal/routes/userRoutes.go
+++ internal/routes/userRoutes.go
@@ -2,11 +2,25 @@ package routes
 
 import (
 	"github.com/FiraBro/local-go/internal/handlers"
-
+	"github.com/FiraBro/local-go/internal/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupAuthRoutes(r *gin.Engine, authHandler *handlers.AuthHandler) {
-	r.POST("/register", authHandler.Register)
-	r.POST("/login", authHandler.Login)
+// Accept *gin.RouterGroup instead of *gin.Engine
+func AuthRoutes(rg *gin.RouterGroup, authHandler *handlers.AuthHandler) {
+	rg.POST("/auth/register", authHandler.Register)
+	rg.POST("/auth/login", authHandler.Login)
+	rg.POST("/auth/refresh", authHandler.Refresh)
+	rg.POST("/auth/logout", authHandler.Logout)
+	rg.POST("/auth/forgot-password", authHandler.ForgotPassword)
+	rg.POST("/auth/reset-password", authHandler.ResetPassword)
+
+	// Protected routes
+	authProtected := rg.Group("/")
+	authProtected.Use(middlewares.AuthMiddleware())
+	{
+		authProtected.GET("/auth/profile", authHandler.GetProfile)
+		authProtected.PATCH("/auth/profile", authHandler.UpdateProfile)
+		authProtected.PATCH("/auth/change-password", authHandler.ChangePassword)
+	}
 }
