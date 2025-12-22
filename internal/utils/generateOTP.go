64bca06724feb55f84b83ce37f645e4a115d487a
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-22 11:10:53 +0300

    add otp generate  utility function

---
+++ internal/utils/generateOTP.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
+func GenerateOTP() string {
+	n, _ := rand.Int(rand.Reader, big.NewInt(900000))
+	return fmt.Sprintf("%06d", n.Int64()+100000)
+}
