133a57cf3e1f36a603a293eba859c3d66081a0ec
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-23 17:24:50 +0300

    group them

--- internal/routes/eventRoutes.go
+++ internal/routes/eventRoutes.go
@@ -3,19 +3,20 @@ package routes
 import (
 	"github.com/FiraBro/local-go/internal/handlers"
 	"github.com/FiraBro/local-go/internal/middlewares"
+	"github.com/FiraBro/local-go/internal/repositories"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Accept *gin.RouterGroup instead of *gin.Engine
-func SetupEventRoutes(rg *gin.RouterGroup, eventHandler *handlers.EventHandler) {
+// SetupEventRoutes sets up public and protected event routes
+func SetupEventRoutes(rg *gin.RouterGroup, eventHandler *handlers.EventHandler, userRepo *repositories.UserRepository) {
 	// Public routes
 	rg.GET("/events", eventHandler.GetEvents)
 	rg.GET("/events/:id", eventHandler.GetEventByID)
 
-	// Protected routes
+	// Protected routes (requires authentication)
 	authGroup := rg.Group("/")
-	authGroup.Use(middlewares.AuthMiddleware())
+	authGroup.Use(middlewares.AuthMiddleware(userRepo))
 	{
 		authGroup.POST("/events", eventHandler.CreateEvent)
 		authGroup.PUT("/events/:id", eventHandler.UpdateEvent)
