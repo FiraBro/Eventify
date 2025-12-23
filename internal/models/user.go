5912a1df7ee49ad721723f0cae646403588192ef
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-23 17:27:20 +0300

    add some field to it

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,9 +1,15 @@
 package models
 
+import "time"
+
 type User struct {
-	ID       string `json:"id"`
-	Username string `json:"username" binding:"required"`
-	Email string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"` // hashed
-	Role     string `json:"role"`                        // e.g., "admin" or "user"
+	ID              string     `json:"id"`
+	Username        string     `json:"username"`
+	Email           string     `json:"email"`
+	Password        string     `json:"password"` // allow JSON binding
+	Role            string     `json:"role"`
+	CreatedAt       time.Time  `json:"created_at"`
+	DeletedAt       *time.Time `json:"deleted_at"`
+	DeleteDeadline  *time.Time `json:"delete_deadline"`
 }
+
