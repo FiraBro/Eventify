5e3e6ac728ee5fe6f0b1240663319f8ab34edb3f
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-23 17:20:39 +0300

    comment for now

--- internal/config/email.go
+++ internal/config/email.go
@@ -1,11 +1,14 @@
-// config/email.go
+// // config/email.go
+// package config
+
+// import "os"
+
+// var (
+//
+//	SMTPHost = os.Getenv("SMTP_HOST")
+//	SMTPPort = os.Getenv("SMTP_PORT")
+//	SMTPUser = os.Getenv("SMTP_USER")
+//	SMTPPass = os.Getenv("SMTP_PASS")
+//
+// )
 package config
-
-import "os"
-
-var (
-	SMTPHost = os.Getenv("SMTP_HOST")
-	SMTPPort = os.Getenv("SMTP_PORT")
-	SMTPUser = os.Getenv("SMTP_USER")
-	SMTPPass = os.Getenv("SMTP_PASS")
-)
