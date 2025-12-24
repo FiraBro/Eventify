d3612d4fb622297f8e636ec03cf5b6fdeeac39c6
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-25 00:02:21 +0300

    creating staff model

---
+++ internal/models/staff.go
@@ -0,0 +1,9 @@
+package models
+
+type Staff struct {
+    ID        string `json:"id"`
+    Name      string `json:"name"`
+    Email     string `json:"email"`
+    Phone     string `json:"phone"`
+    Role      string `json:"role"`
+}
