82089c1ac6807bc1c563ad26a9cd75f9a2003b8f
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-25 21:40:45 +0300

    igrate it

--- cmd/migrate/migration/000006_staff_service_table.up.sql
+++ cmd/migrate/migration/000006_staff_service_table.up.sql
@@ -1,5 +1,7 @@
-CREATE TABLE IF NOT EXISTS staff_services (
+-- Re-create it with service_id instead of service_name
+CREATE TABLE staff_services (
     id UUID PRIMARY KEY,
     staff_id UUID NOT NULL REFERENCES staff(id) ON DELETE CASCADE,
-    service_name TEXT NOT NULL
+    service_id UUID NOT NULL REFERENCES services(id) ON DELETE CASCADE,
+    UNIQUE(staff_id, service_id)
 );
